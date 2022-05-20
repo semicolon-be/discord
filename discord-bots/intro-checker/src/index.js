245a2bfbfa1f6172e5a5e8bd64e0f80842f4e270
Julien Desktop <julien.vanbeveren@gmail.com>
2022-05-20 18:09:06 +0200

    messageUpdate init -> not working

--- discord-bots/intro-checker/src/index.js
+++ discord-bots/intro-checker/src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const { Client, Intents } = require('discord.js')
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] })
 
 client.login(process.env.DISCORDJS_BOT_TOKEN)
 
@@ -9,8 +9,14 @@ client.on('ready', () => {
     console.log('running')
 })
 
-client.on('messageCreate', async (message) => {
+client.on('messageCreate', (message) => {
     handleIntroMessage(message)
+})
+
+client.on('messageUpdate', (oldMessage, newMessage) => {
+    console.log("message updated")
+    return
+    handleIntroMessage(newMessage)
 })
 
 
@@ -19,12 +25,12 @@ async function handleIntroMessage(message) {
         return
     }
 
-    const validate = /name:\s[\w\s]+(\r\n|\r|\n)year:\s\d{4}/gm
+    const validate = /name:\s[\w\s]+(\r\n|\r|\n)(year:\s\d{4})?(\r\n|\r|\n)about:\s.{1,50}(\r\n|\r|\n)languages:\s(.{1,10}[,(\r\n|\r|\n)]\s?)+^(portfolio|website):\shttps?:\/\/\w{1,20}\.\w{1,20}\.?(\w{1,20})?\/?.{0,30}/gm
 
     if (message.content.match(validate)) {
         console.log('correct format')
         console.log(message.channel)
-        client.channels.get('977092731178455040').overwritePermissions(message.author, { SEND_MESSAGES: false })
+        // client.channels.get('977092731178455040').overwritePermissions(message.author, { SEND_MESSAGES: false })
         return
     }
     message.delete()
